98e487124335157326704d01f28adbd78263af4c
DESKTOP-12JO8K5\Juanma <jsangam2606@iescarrillo.es>
2026-04-27 10:57:29 +0200

    Add environment files for production

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  anthropicApiKey: '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',
+  backendUrl: 'https://huerting-backend.onrender.com'
+};
