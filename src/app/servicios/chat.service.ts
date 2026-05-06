bf7510faa08f3fa6ee4bfb5d86f7f13147b7f431
DESKTOP-12JO8K5\Juanma <jsangam2606@iescarrillo.es>
2026-05-06 13:15:47 +0200

    no message

--- src/app/servicios/chat.service.ts
+++ src/app/servicios/chat.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 export interface Mensaje {
   role: 'user' | 'assistant';
